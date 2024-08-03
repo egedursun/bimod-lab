f3b9c2efa05d74a34af774bcc482f042ce57f667
egedursun <esa.ege@gmail.com>
2024-08-03 21:05:48 +0300

    production server updates.

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -28,9 +28,6 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 # Database migrations
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py makemigrations
 python3 manage.py migrate
 
 # Collect static files
