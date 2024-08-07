2b1a72f159473487b306f967057e4c79ac574c09
egedursun <esa.ege@gmail.com>
2024-08-07 22:49:45 +0300

    updates for integration tests.

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,10 +3,18 @@
 # Stop on any error
 set -e
 
+echo "          [Deploy / Development] Deployment started!"
+
+echo "          [Deploy / Development] Checking for root privileges..."
+
 # Define the directory
 DEPLOY_DIR=/var/www/bimod_dev
 REPO_URL=https://github.com/Bimod-HQ/bimod-app.git
 BRANCH=$1
+
+echo "          [Deploy / Development] Root privileges checked!"
+
+echo "          [Deploy / Development] Cloning or updating the repository..."
 
 # Clone or update the repo
 if [ ! -d "$DEPLOY_DIR" ]; then
@@ -18,40 +26,94 @@ else
   sudo git pull origin dev
 fi
 
+echo "          [Deploy / Development] Repository cloned or updated!"
+
+echo "          [Deploy / Development] Relocating to the directory..."
+
 # Change to the directory
 cd $DEPLOY_DIR
+
+echo "          [Deploy / Development] Relocated to the directory!"
+
+echo "          [Deploy / Development] Creating virtual environment..."
 
 # Activate the virtual environment
 source venv/bin/activate
 
+echo "          [Deploy / Development] Virtual environment created!"
+
+echo "          [Deploy / Development] Installing requirements..."
+
 # Install requirements
 pip install -r requirements.txt
+
+echo "          [Deploy / Development] Requirements installed!"
+
+echo "          [Deploy / Development] Running database migrations..."
 
 # Database migrations
 python3 manage.py migrate
 
+echo "          [Deploy / Development] Database migrations completed!"
+
+echo "          [Deploy / Development] Collecting static files..."
+
 # Collect static files
 python3 manage.py collectstatic --noinput
 
+echo "          [Deploy / Development] Static files collected!"
+
+echo "          [Deploy / Development] Restarting services..."
+
 if ! sudo systemctl is-active --quiet redis; then
-    echo "[Deploy / Development] Redis is not active. Starting Redis..."
+    echo "          [Deploy / Development] Redis is not active. Starting Redis..."
     sudo systemctl start redis
+    echo "          [Deploy / Development] Redis started!"
 fi
+
+
+echo "          [Deploy / Development] Granting permissions..."
 
 # Restart services
 sudo mkdir -p /run/celery
 sudo chown www-data:www-data /run/celery
 sudo chmod 755 /run/celery
 
+echo "          [Deploy / Development] Restarting services..."
+
 systemctl daemon-reload
+
+echo "          [Deploy / Development] Reloaded daemon!"
+
 sudo systemctl restart redis
+
+echo "          [Deploy / Development] Restarted Redis!"
+
 sudo systemctl restart gunicorn_dev
+
+echo "          [Deploy / Development] Restarted Gunicorn!"
+
 sudo systemctl restart nginx
+
+echo "          [Deploy / Development] Restarted Nginx!"
+
 sudo systemctl restart celery_dev
+
+echo "          [Deploy / Development] Restarted Celery!"
+
 sudo systemctl restart celerybeat_dev
+
+echo "          [Deploy / Development] Restarted Celery Beat!"
+
 sudo systemctl restart flower_dev
 
-echo "[Deploy / Development] Deployment successful!"
+echo "          [Deploy / Development] Restarted Flower!"
+
+echo "[Deploy / Development] Running integration tests..."
 
 # Run the integration tests
-pytest
+pytest -v
+
+echo "[Deploy / Development] Integration tests are completed successfully!"
+
+echo "[Deploy / Development] Deployment completed successfully!"
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,10 +3,18 @@
 # Stop on any error
 set -e
 
+echo "          [Deploy / Production] Deployment started!"
+
+echo "          [Deploy / Production] Checking for root privileges..."
+
 # Define the directory
 DEPLOY_DIR=/var/www/bimod_prod
 REPO_URL=https://github.com/Bimod-HQ/bimod-app.git
 BRANCH=$1
+
+echo "          [Deploy / Production] Root privileges checked!"
+
+echo "          [Deploy / Production] Cloning or updating the repository..."
 
 # Clone or update the repo
 if [ ! -d "$DEPLOY_DIR" ]; then
@@ -18,40 +26,93 @@ else
   sudo git pull origin main
 fi
 
+echo "          [Deploy / Production] Repository cloned or updated!"
+
+echo "          [Deploy / Production] Relocating to the directory..."
+
 # Change to the directory
 cd $DEPLOY_DIR
+
+echo "          [Deploy / Production] Relocated to the directory!"
+
+echo "          [Deploy / Production] Creating virtual environment..."
 
 # Activate the virtual environment
 source venv/bin/activate
 
+echo "          [Deploy / Production] Virtual environment created!"
+
+echo "          [Deploy / Production] Installing requirements..."
+
 # Install requirements
 pip install -r requirements.txt
+
+echo "          [Deploy / Production] Requirements installed!"
+
+echo "          [Deploy / Production] Running database migrations..."
 
 # Database migrations
 python3 manage.py migrate
 
+echo "          [Deploy / Production] Database migrations completed!"
+
+echo "          [Deploy / Production] Collecting static files..."
+
 # Collect static files
 python3 manage.py collectstatic --noinput
 
+echo "          [Deploy / Production] Static files collected!"
+
+echo "          [Deploy / Production] Restarting services..."
+
 if ! sudo systemctl is-active --quiet redis; then
-    echo "[Deploy / Production] Redis is not active. Starting Redis..."
+    echo "          [Deploy / Production] Redis is not active. Starting Redis..."
     sudo systemctl start redis
+    echo "          [Deploy / Production] Redis started!"
 fi
+
+echo "          [Deploy / Production] Granting permissions..."
 
 # Restart services
 sudo mkdir -p /run/celery
 sudo chown www-data:www-data /run/celery
 sudo chmod 755 /run/celery
 
+echo "          [Deploy / Production] Restarting services..."
+
 systemctl daemon-reload
+
+echo "          [Deploy / Production] Reloaded daemon!"
+
 sudo systemctl restart redis
+
+echo "          [Deploy / Production] Restarted Redis!"
+
 sudo systemctl restart gunicorn_prod
+
+echo "          [Deploy / Production] Restarted Gunicorn!"
+
 sudo systemctl restart nginx
+
+echo "          [Deploy / Production] Restarted Nginx!"
+
 sudo systemctl restart celery_prod
+
+echo "          [Deploy / Production] Restarted Celery!"
+
 sudo systemctl restart celerybeat_prod
+
+echo "          [Deploy / Production] Restarted Celery Beat!"
+
 sudo systemctl restart flower_prod
+
+echo "          [Deploy / Production] Restarted Flower!"
 
 echo "[Production / Deploy] Deployment successful!"
 
 # Run the integration tests
 pytest
+
+echo "[Production / Deploy] Integration tests are completed successfully!"
+
+echo "[Production / Deploy] Deployment completed successfully!"
