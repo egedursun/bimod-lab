027d50672470a2b0858d3fb399d920ce6df8dfc3
egedursun <esa.ege@gmail.com>
2024-08-20 05:40:08 +0300

    redis integration for the web sockets.

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ load_dotenv()  # take environment variables from .env.
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-APPEND_SLASH = True
+# APPEND_SLASH = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
